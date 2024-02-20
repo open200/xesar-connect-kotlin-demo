10310e9964e415a228e3292104534e32931ae56c
Martin Pfeffer <39983062+MartinPfeffer@users.noreply.github.com>
2024-02-20 18:04:51 +0100

    build: increase xesar-connect version

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,16 +13,17 @@ group = "com.open200"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("io.github.microutils:kotlin-logging:3.0.5")
-    implementation("com.open200:xesar-connect:0.5.0")
+    implementation("com.open200:xesar-connect:0.9.0")
 
     // add SLF4J implementation of your choice (e.g. logback)
-    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("ch.qos.logback:logback-classic:1.4.14")
 
     // add security provider of your choice (e.g. bouncycastle)
     implementation("org.bouncycastle:bcprov-jdk18on:1.76")
